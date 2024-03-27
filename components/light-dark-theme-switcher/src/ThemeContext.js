bfb493df9a173f4fdac0179385fa9fca80f24396
Shadrackssien <shadrack6960@gmail.com>
2024-03-27 13:29:54 +0000

    Adding light- dark theme

---
+++ components/light-dark-theme-switcher/src/ThemeContext.js
@@ -0,0 +1,18 @@
+import {createContext, useState, useContext} from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState("Light")
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+    }
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
+
