fd9b40aa587d7deb6e047564ac290cc983612bb7
Shadrackssien <shadrack6960@gmail.com>
2024-03-28 06:32:19 +0000

    Added Goal App

--- react-and-customs-hooks/goals-app-useState-example/src/App.js
+++ react-and-customs-hooks/goals-app-useState-example/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function GoalForm(props){
+
+  const [formData, setFormData] = React.useState({
+    goal: "",
+    by: ""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  const submitHandler  = (e) => {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({goal: " ", by: " "});
+  };
+  return (
+    <>
+      <h1>My Little Lemon Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input 
+          type="text" 
+          name="goal" 
+          placeholder="Goal" 
+          value={FormData.goal}
+          onChange={changeHandler} />
+          <input 
+          type="text" 
+          name="by" 
+          placeholder="By..." 
+          value={FormData.by}
+          onChange={changeHandler} />
+          <button>Submit Goal</button>
+      </form>
+    </>
+  )
+}
+
+function ListOfGoals(props){
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>My goal is to {g.goal}, by {g.by}</span>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 function App() {
+  const [allGoals, updateAllGoals] = React.useState([]);
+
+  const addGoal = (goal) => {
+    updateAllGoals([...allGoals, goal])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <GoalForm onAdd={addGoal} />
+    <ListOfGoals allGoals={allGoals}/>
+  </div>
+  )
 }
 
 export default App;
