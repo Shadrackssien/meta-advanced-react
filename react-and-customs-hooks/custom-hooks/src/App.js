3543de0862b3907881ceea322a1fc12b690daed9
Shadrackssien <shadrack6960@gmail.com>
2024-03-29 13:43:15 +0000

    usePrevious custom hook

--- react-and-customs-hooks/custom-hooks/src/App.js
+++ react-and-customs-hooks/custom-hooks/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const usePrevious = (val) => {
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current = val;
+  }, [val])
+
+  return ref.current;
+}
 
 function App() {
+  const [day, setDay] = React.useState("Monday");
+  const prevDay = usePrevious(day);
+
+  const handleClick = () => {
+    if (day === "Monday") {
+      setDay("Tuesday")
+    }else if (day === "Tuesday"){
+      setDay("Wednesday")
+    }else if (day === "Wednesday"){
+      setDay("Thursday")
+    }else if (day === "Thursday"){
+      setDay("Friday")
+    }else if (day === "Friday"){
+      setDay("Saturday")
+    }else if (day === "Saturday"){
+      setDay("Sunday")
+    }else if (day === "Sunday"){
+      setDay("Monday")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Today is {day} </h1>
+      {prevDay && (<p>Previous work day was: {prevDay}</p>)}
+      <button onClick={handleClick}>
+        Get next day
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
